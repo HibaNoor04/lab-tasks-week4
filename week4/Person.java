5e459f99264b2c57beb948270b625f9420385c82
HibaNoor04 <sp24-bse-044@cuilahore.edu.pk>
2024-10-24 11:26:11 +0500

    Person.java

--- week4/Person.java
+++ week4/Person.java
@@ -1,1 +1,18 @@
-public class Person{
+public class Person{ public class Person {
+  private  String name;
+ private String email;
+ public Person(String name,String email)
+{this.name=name;
+this.email=email;}
+public void setName(String name){
+this.name=name;}
+public String getName(){
+return name;}
+public void setEmail(String email){
+this.email=email;}
+public String getEmail(){
+return email;}
+//toString:
+public String toString(){
+return String.format("%s %s",name,email);}
+}
